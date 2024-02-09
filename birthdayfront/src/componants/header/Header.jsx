68fb0ac0770d68d42b5901618718f3ef51143bcb
clement bernard <c.bernard817@laposte.net>
2024-02-09 14:46:29 +0100

    header finito

---
+++ birthdayfront/src/componants/header/Header.jsx
@@ -0,0 +1,35 @@
+import React, { useState }  from 'react'
+import './Header.scss'
+
+
+
+function getDate() {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  return `${date}/${month}/${year}`;
+}
+
+const Header = () => {
+  const [currentDate, setCurrentDate] = useState(getDate());
+
+  return (
+    <div>
+      <div class="header">
+        <div>
+          CITATION ET ANNIVERSAIRE
+        </div>
+        <div>
+          {currentDate}
+        </div>
+      </div>
+      <hr/>
+    </div>
+
+
+    
+  )
+}
+
+export default Header
