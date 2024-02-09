0c37d8c41a02b97284c1336f8a393d508639140a
clement bernard <c.bernard817@laposte.net>
2024-02-09 16:58:37 +0100

    colonen de gauche

--- birthdayfront/src/componants/ColGauche/ColGauche.jsx
+++ birthdayfront/src/componants/ColGauche/ColGauche.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './ColGauche.scss'
 
+import rond from '../../assets/rond.png'
+
 function ColGauche() {
   return (
     <div class="bodyG">
-      <div >
-        <p>hjkcvbn,;:</p>
-      </div>
+        <div class='divImg'>
+            <img class="img" src={rond} alt="hb qui tourne" />  
+        </div>
+        <div class="txtNom">
+            NOM PRENOM
+        </div>
+        
     </div>
   )
 }
